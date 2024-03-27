6b8810b31ac8b043a6735102c2092030c065f278
AdrianaOrbai <orbai.roxana@gmail.com>
2024-03-27 19:42:27 +0200

    added also option to delete question for admin

--- admin_functions.py
+++ admin_functions.py
@@ -24,10 +24,26 @@ def add_question(all_questions: list, questions_path: str = "questions.json"):
     except Exception as e:
         print(f"Error on adding a new question {e}")
 
-def delete_question():
-    pass
 
+def delete_question(all_questions: list, questions_path: str = "questions.json"):
+    try:
 
+        questions_list = [question['question'] for question in all_questions]
+        for question in questions_list:
+            print(f"{questions_list.index(question)}.{question}")
+
+        delete_question = input("Introdu index-ul intrebarii pe care doresti sa o stergi: ")
+        all_questions.pop(delete_question, None)
+
+        with open(questions_path, "w") as f:
+            f.write(json.dumps({"questions": all_questions}, indent=4))
+    except IndexError as e:
+        print(f"Question not found. Error on deleting: {e}")
+    except Exception as e:
+        print(f"Unknown Error on deleting: {e} ")
+
+    else:
+        print("Successfully deleted question.")
 
 def change_correct_answer():
     pass
@@ -41,7 +57,7 @@ def run():
         case "1":
             add_question(questions)
         case "2":
-            pass
+            delete_question(questions)
         case _:
             exit()
 
